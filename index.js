018c28120146ba873a9fecd6dfbf4ff4e8148a13
Olafur <olafuringieinarsson@gmail.com>
2021-11-28 14:20:32 +0100

    Setting up file structure

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 const express = require("express");
 const app = express();
 
+
+
+// Define ther server PORT and listen
+const PORT = 4000;
+
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT );
+})
+
